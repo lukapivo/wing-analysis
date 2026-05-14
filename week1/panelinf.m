ad054fa8fcf0c8d062bb18ecd5a5dc5685602c6d
snatchingteeth <camillaoj047@gmail.com>
2026-05-14 12:55:21 +0100

    Update panelinf.m

--- week1/panelinf.m
+++ week1/panelinf.m
@@ -1,6 +1,18 @@
 function [infa infb] = panelinf(xa, ya, xb, yb, x, y)
 % PANELINF Compute influence coefficients of a general location panel.
 
-t = 
+r = sqrtm((x-xa)^2 + (y-ya)^2);
+
+tl = [xb-xa yb-ya];
+del = norm(tl);
+t = tl / del;
+
+nl = [-(yb-ya) xb - xa];
+n = nl / norm(nl);
+
+X = dot(r,t);
+Yin = dot(r,n);
+
+[infa, infb] = refpaninf(del,X,Yin);
 
 end
