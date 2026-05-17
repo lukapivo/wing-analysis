3dffaa6f9fa49070e68f6a595151ce7ce65cb615
snatchingteeth <camillaoj047@gmail.com>
2026-05-17 12:15:07 +0100

    Fixed panelinf

--- week1/panelinf.m
+++ week1/panelinf.m
@@ -1,19 +1,21 @@
 function [infa infb] = panelinf(xa, ya, xb, yb, x, y)
 % PANELINF Compute influence coefficients of a general location panel.
 
-r = [x-xa y-ya];
-r
+% x and y are matrices
+dx = x - xa;
+dy = y - ya;
 
+% Computes tangent and normal vectors to the panel
 tl = [xb-xa yb-ya];
 del = norm(tl);
 t = tl / del;
-t
 
 nl = [-(yb-ya) xb - xa];
 n = nl / norm(nl);
 
-X = dot(r,t,1);
-Yin = dot(r,n,1);
+% Projections onto tangent and normal
+X  = dx * t(1) + dy * t(2);   % matrix, same size as x
+Yin = dx * n(1) + dy * n(2);
 
 [infa, infb] = refpaninf(del,X,Yin);
 
