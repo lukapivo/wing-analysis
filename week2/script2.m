aa253b02fe9244c4ea3e7a6baa2ab1e6252daeeb
lukapivo <152909849+lukapivo@users.noreply.github.com>
2026-05-21 12:46:28 +0100

    Do exercise 2 (not the tabulation)

--- week2/script2.m
+++ week2/script2.m
@@ -1,19 +1,24 @@
 % Script 1
-% Zero pressure gradient laminar boundary layer
+% Constant velocity gradient laminar boundary layer with test for
+% transition
 
 clear;
 close all;
 
 
 % Set the Reynolds number
-Re_L = 1000;
+Re_L = 5e6;
+
+% Velocity gradient
+due_dx = -0.1;
 
 % Panel count
 n = 100;
+laminar = true;
 
 % Dimensionless x/L and ue/U
 x = linspace(0,1,n + 1);
-ue = ones(size(x));
+ue = ones(size(x)) + due_dx .* x;
 % True for zero pressure gradient
 
 % Blasius solution
@@ -23,10 +28,25 @@ thetas_blas = 0.664 / Re_L^(1/2) .* x.^(1/2);
 thetas = zeros(size(x));
 ueint = 0;
 
-for i=2:(n+1)
+i = 1;
+while laminar && i < n
+    i = i + 1;
+    
     ueint = ueint + ueintbit(x(i-1),ue(i-1),x(i),ue(i));
     theta_sq = 0.45 / Re_L * (ue(i))^(-6) * ueint;
     thetas(i) = sqrt(theta_sq);
+    
+    % Test for transition
+    m = - Re_L * theta_sq * due_dx;
+    H = thwaites_lookup(m);
+    He = laminar_He(H);
+    Rethet = Re_L * ue(i) * thetas(i);
+
+    if log(Rethet) >= 18.4*He - 21.74
+        laminar = false;
+        disp([x(i) Rethet/1000])
+    end
+    
 end
 
 
@@ -42,7 +62,7 @@ hold off
 
 legend('Thwaites solution','Blasius solution')
 
-print -deps2c -loose week2/Figures/script1.eps
+print -deps2c -loose week2/Figures/script2.eps
 
 
 
