4f9406b0466ef6e2d0f4e7919b81442e4e1907ea
lukapivo <152909849+lukapivo@users.noreply.github.com>
2026-05-28 00:11:43 +0100

    Cut whitespace in script1

--- week2/script1.m
+++ week2/script1.m
@@ -4,31 +4,26 @@
 clear;
 close all;
 
-
 % Set the Reynolds number
 Re_L = 1000;
 
 % Panel count
 n = 100;
 
-% Dimensionless x/L and ue/U
+% Dimensionless x/L, ue/U, theta/L
 x = linspace(0,1,n + 1);
-ue = ones(size(x));
-% True for zero pressure gradient
+ue = ones(size(x)); % True for zero pressure gradient
+thetas = zeros(size(x));
 
 % Blasius solution
 thetas_blas = 0.664 / Re_L^(1/2) .* x.^(1/2);
 
-% Dimensionless theta/L
-thetas = zeros(size(x));
 ueint = 0;
-
 for i=2:(n+1)
     ueint = ueint + ueintbit(x(i-1),ue(i-1),x(i),ue(i));
     theta_sq = 0.45 / Re_L * (ue(i))^(-6) * ueint;
     thetas(i) = sqrt(theta_sq);
 end
-
 
 % Momentum thickness plot
 set(gcf,'units', 'centimeters','position',[0,0,16,10])
