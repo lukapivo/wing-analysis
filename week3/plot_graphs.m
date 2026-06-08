4830be12aa283d711ff57984ef68d58577d1f103
snatchingteeth <camillaoj047@gmail.com>
2026-06-08 17:13:52 +0100

    Added figures

--- week3/plot_graphs.m
+++ week3/plot_graphs.m
@@ -1,7 +1,7 @@
 clear
 close all
 
-caseref = "Data/LLL08b.mat";
+caseref = "Data/LLL05.mat";
 load(caseref);
 
 % C_L alpha and C_D alpha
--- week3/resstd.m
+++ week3/resstd.m
@@ -15,7 +15,7 @@ axis('equal')
 xlabel('x/c')
 ylabel('y/c')
 title('Van de Vooren aerofoil')
-saveas(gfc,'week3/Figures/resstd_airfoil','epsc')
+saveas(gcf,'week3/Figures/resstd_airfoil','epsc')
 
 disp('Starting 100 panel calculation ...')
 np = 100;
@@ -67,29 +67,34 @@ xlabel('x/c')
 ylabel('-c_p')
 title('Van de Vooren cps; varying panel size')
 legend('exact','100pans','200pans','400pans','800pans')
-saveas(gfc,'week3/Figures/resstd_all','epsc')
+saveas(gcf,'week3/Figures/resstd_all','epsc')
 
 figure(3)
 plot(xsin,-cpex,xs1,-cp1,'--')
+
+ax = axes();
+zoom_region = [0 0 1 1];
+zoom_ax = zoomed_axes(ax, zoom_region);
+
 legend('exact','100pans')
-saveas(gfc,'week3/Figures/resstd_100','epsc')
+saveas(gcf,'week3/Figures/resstd_100','epsc')
 
 figure(4)
 plot(xsin,-cpex,xs2,-cp2,'-.')
 legend('exact','200pans')
-saveas(gfc,'week3/Figures/resstd_200','epsc')
+saveas(gcf,'week3/Figures/resstd_200','epsc')
 
 figure(5)
 plot(xsin,-cpex,xs4,-cp4,'-+')
 legend('exact','400pans')
-saveas(gfc,'week3/Figures/resstd_400','epsc')
+saveas(gcf,'week3/Figures/resstd_400','epsc')
 
 figure(6)
 plot(xsin,-cpex,xs8,-cp8,'-x')
 legend('exact','800pans')
-saveas(gfc,'week3/Figures/resstd_800','epsc')
+saveas(gcf,'week3/Figures/resstd_800','epsc')
 
 figure(7)
 plot(xsin,-cpex)
 legend('exact')
-saveas(gfc,'week3/Figures/resstd_exact','epsc')
+saveas(gcf,'week3/Figures/resstd_exact','epsc')
