3c28dffe9cf7f7441aeae5740ea73d6b17a76925
snatchingteeth <camillaoj047@gmail.com>
2026-06-10 00:29:22 +0100

    cp airfoil plots

---
+++ plot_airfoil_cp.m
@@ -0,0 +1,404 @@
+clear
+close all
+
+function [] = plot_points(int, ils, itr, its, ax1, x, thetas, color)
+C = color;
+
+xl1 = xlim(ax1);
+yl1 = ylim(ax1);
+
+dx1 = (xl1(2) - xl1(1)) * 0.005;
+dy1 = (yl1(2) - yl1(1)) * 0.005;
+
+if int ~= 0
+    disp(['Natural transition at ' num2str(x(int))])
+    plot(ax1, x(int), thetas(int), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    text(ax1, x(int)-2*dx1, thetas(int)+2*dy1, "NT", "FontSize", 10, "Color", C, "HorizontalAlignment", "Right")
+    % plot(ax2, x(int), He(int), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    % text(ax2, x(int)-2*dx2, He(int)+2*dy2, "Natural Transition", "FontSize", 10, "Color", C, "HorizontalAlignment", "Right")
+end
+
+if ils ~= 0
+    disp(['Laminar separation at ' num2str(x(ils))])
+    plot(ax1, x(ils), thetas(ils), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    text(ax1, x(ils)-2*dx1, thetas(ils)+2*dy1, "LS", "FontSize", 10, "Color", C, "HorizontalAlignment", "Right")
+    % plot(ax2, x(ils), He(ils), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    % text(ax2, x(ils)+dx2, He(ils), "Laminar Separation", "FontSize", 10, "Color", C)
+end
+
+if itr ~= 0
+    disp(['Turbulent reattachment at ' num2str(x(itr))])
+    plot(ax1, x(itr), thetas(itr), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    text(ax1, x(itr)+dx1, thetas(itr)-2*dy1, "TR", "FontSize", 10, "Color", C)
+    % plot(ax2, x(itr), He(itr), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    % text(ax2, x(itr)+dx2, He(itr), "Turbulent Reattachment", "FontSize", 10, "Color", C)
+end
+
+if its ~= 0
+    disp(['Turbulent separation at ' num2str(x(its))])
+    plot(ax1, x(its), thetas(its), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    text(ax1, x(its)-2*dx1, thetas(its)+2*dy1, "TS", "FontSize", 10, "Color", C, "HorizontalAlignment", "Right")
+    % plot(ax2, x(its), He(its), ".", "MarkerSize",10, "Color", C,'HandleVisibility','off')
+    % text(ax2, x(its)-2*dx2, He(its), "Turbulent Separation", "FontSize", 10, "Color", C, "HorizontalAlignment", "Right")
+end
+end
+
+caseref = "Data/NACA0012_1.mat";
+load(caseref);
+
+% Airfoils
+figure(1);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+
+caseref = "Data/LLL_1.mat";
+load(caseref);
+
+
+figure(1);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('NACA0012', 'LLL01')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL01','epsc2')
+
+
+figure(2);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL02_1.mat";
+load(caseref);
+
+
+figure(2);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL01', 'LLL02')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL02','epsc2')
+
+figure(3);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL03_1.mat";
+load(caseref);
+
+
+figure(3);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL02', 'LLL03')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL03','epsc2')
+
+figure(4);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL04_1.mat";
+load(caseref);
+
+
+figure(4);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL03', 'LLL04')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL04','epsc2')
+
+figure(5);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL05_1.mat";
+load(caseref);
+
+
+figure(5);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL04', 'LLL05')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL05','epsc2')
+
+figure(6);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL06_1.mat";
+load(caseref);
+
+
+figure(6);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL05', 'LLL06')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL06','epsc2')
+
+figure(7);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL07_1.mat";
+load(caseref);
+
+
+figure(7);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL06', 'LLL07')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL07','epsc2')
+
+figure(8);
+box on 
+hold on
+plot(xs, cp, 'b');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'b');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'b');
+
+caseref = "Data/LLL08_1.mat";
+load(caseref);
+
+
+figure(8);
+box on 
+hold on
+plot(xs, cp, 'r');
+set(gca, 'Ydir', 'reverse')
+xlabel('s');
+ylabel('c_p');
+
+xsu = flip(xs(1:length(su)));
+cpu = flip(cp(1:length(su)));
+xsl = xs(length(su)+1:end);
+cpl = cp(length(su)+1:end);
+
+ax_cp = gca;
+
+plot_points(ilnt, ills, iltr, ilts, ax_cp, xsl, cpl, 'r');
+plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
+
+legend('LLL07', 'LLL08')
+set(gcf,'units', 'centimeters','position',[0,0,8,5])
+saveas(gcf,'week3/Figures/cp_LLL08','epsc2')
+
+
