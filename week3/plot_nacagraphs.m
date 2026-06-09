e100dc6f1850b7095b580964bcbaa5a25c825d0c
snatchingteeth <camillaoj047@gmail.com>
2026-06-09 17:44:55 +0100

    Fixed legends

--- week3/plot_nacagraphs.m
+++ week3/plot_nacagraphs.m
@@ -13,6 +13,8 @@ alphas_cd = interp1(cl_alpha.Var2, cl_alpha.Var1, cd_cl.Var1);
 
 % C_L alpha
 figure(1);
+grid on
+box on
 hold on
 plot(alpha, clswp);
 scatter(cl_alpha.Var1, cl_alpha.Var2, 'X')
@@ -21,19 +23,21 @@ scatter(cl_alpha.Var1, cl_alpha.Var2, 'X')
 xlabel('\alpha (degrees)');
 ylabel('C_L');
 title('NACA 4412');
-legend('Panel result', 'Experiment')
+legend('Panel result', 'Experiment', 'Location','northwest')
 set(gcf,'units', 'centimeters','position',[0,0,8,5])
 saveas(gcf,'week3/Figures/naca4412_clalpha','epsc2')
 hold off;
 
 figure(2);
+grid on
+box on
 hold on
 plot(clswp, cdswp)
 scatter(cd_cl.Var1, cd_cl.Var2, 'X')
 xlabel('C_L');
 ylabel('C_D');
 title('NACA 4412');
-legend('Panel result', 'Experiment')
+legend('Panel result', 'Experiment','Location','north')
 ylim([0,0.055]);
 set(gcf,'units', 'centimeters','position',[0,0,8,5])
 saveas(gcf,'week3/Figures/naca4412_clcd','epsc2')
@@ -41,12 +45,14 @@ hold off;
 
 figure(3);
 grid on
+box on
 hold on
 plot(alpha, cdswp);
 scatter(alphas_cd, cd_cl.Var2, 'X')
 xlabel('\alpha (degrees)');
 ylabel('C_D');
 title('NACA 4412');
+legend('Panel result', 'Experiment','Location','north')
 ylim([0,0.055]);
 set(gcf,'units', 'centimeters','position',[0,0,8,5])
 saveas(gcf,'week3/Figures/naca4412_cdalpha','epsc2')
