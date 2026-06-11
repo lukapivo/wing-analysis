72ab95eac949ee3e5e96abd9875a2e640c1b03e3
lukapivo <152909849+lukapivo@users.noreply.github.com>
2026-06-11 13:18:46 +0100

    Fixed plots again

--- plot_airfoil_cp_high.m
+++ plot_airfoil_cp_high.m
@@ -552,4 +552,4 @@ plot_points(iunt, iuls, iutr, iuts, ax_cp, xsu, cpu, 'r');
 
 legend('LLL08', 'Location','northeast')
 set(gcf,'units', 'centimeters','position',[0,0,8,5])
-saveas(gcf,'week3/Figures/cp_LLL08_high.eps','epsc2')
+saveas(gcf,'week3/Figures/cp_LLH01_7.8.eps','epsc2')
